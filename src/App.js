eae3c1396d24df9d3a36fb41179c2428c02d591d
Guy Viner <gregorious@gmail.com>
2017-08-16 14:47:18 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+const owl = {
+  title: "Excellent Owl",
+  src: "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-owl.jpg"
+};
+
+// Component class starts here:
+class Owl extends React.Component {
+  render() {
+      return (
+        <div className="App">
+          <h1 className="App-header">{owl.title}</h1>
+          <img className="App-logo" src={owl.src} alt={owl.title}/>
+        </div>
+      )
+  }
+}
+
+export default Owl;
